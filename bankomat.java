fc66ad0ee1e57c973164c969e9109dfda956dc94
OLYA <vip_1985@tut.by>
2018-11-08 22:56:03 +0300

    First commit

--- bankomat.java
+++ bankomat.java
@@ -1,35 +1,40 @@
 package example.com.hi;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import com.sun.security.auth.SolarisNumericUserPrincipal;
+
+import java.sql.SQLOutput;
 import java.util.LinkedList;
 import java.lang.String;
 import java.util.Scanner;
 
 public class bankomat {
     public static void main(String[] args) {
-
+        String Cash_5 = "5";
+        final int money_5 = 5;
         LinkedList<String> cash5 = new LinkedList< >();
         for (int x = 0; x < 2400; x++) {
-            cash5.add("5");
+            cash5.add(Cash_5);
         }
-        int add = 1;
-        int give = 2;
+        final int ADD = 1;
+        final int GIVE = 2;
         System.out.println("Если вы хотите положить деньги нажмите: 1");
         System.out.println("Если вы хотите снять деньги нажмите: 2");
 
         Scanner in = new Scanner(System.in);
         int L = in.nextInt();
-        if (L == add) {
+        if (L == ADD) {
             System.out.println("Какую сумму вы хотите положить: ");
             int N = in.nextInt();
-            for (int y = 0; y < N / 5; y++) {
-                cash5.add("5");
+            for (int y = 0; y < N / money_5; y++) {
+                cash5.add(Cash_5);
             }
-            System.out.println("Баланс составляет: " + cash5.size() * 5 + "$");
+            System.out.println("Баланс составляет: " + cash5.size() * money_5 + "$");
         }
 
-        if (L == give)
+        if (L == GIVE)
             System.out.println("Сколько денег вы хотите снять? ");
         int k = in.nextInt();
-        int s = cash5.size()*5;
+        int s = cash5.size()*money_5;
 
         if (k > s)
 
